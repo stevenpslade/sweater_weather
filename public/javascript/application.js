f498ba9d92b6986e82c3c9f52dbfb1a085940a1f
stevenpslade <stevenpslade@gmail.com>
2015-11-05 00:36:01 -0800

    using API to get current weather for specific locations

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -28,7 +28,25 @@ $(function() {
   })
 
   $('#searchResults').on('click', 'button.showWeather', function() {
-    alert('works');
+    var apiLink = city.l + '.json';
+    // http://api.wunderground.com/api/3e541a2c882e2d33/conditions/q/CA/San_Francisco.json
+    //http://api.wunderground.com/api/3e541a2c882e2d33/forecast/q/zmw:94125.1.99999.json
+    //3e541a2c882e2d33
+    $.ajax({
+      url: 'http://api.wunderground.com/api/3e541a2c882e2d33/conditions/' + apiLink,
+      method: 'GET',
+      dataType: 'jsonp',
+      // jsonp:    "cb",
+      success:  function (data) {
+        $('#searchResults').empty();
+        var weather = data.current_observation.weather; //string
+        var temp = data.current_observation.temp_c; //number
+        var wind = data.current_observation.wind_kph; //number
+        var feelsLike = data.current_observation.feelslike_c; //string
+        var icon = data.current_observation.icon_url; //link to image
+        console.log(weather);
+      }
+    })
   })
 
 });
