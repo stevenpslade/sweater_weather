059e82d31aa42f2d8e0c61b221100fc8083c5c63
stevenpslade <stevenpslade@gmail.com>
2015-11-04 23:39:17 -0800

    shows search results from API

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,23 +1,34 @@
 $(function() {
-//search request:
-//http://autocomplete.wunderground.com/aq?query=
+
+  function appendResults(city) {
+    $('<button>')
+    .data('link', city.l)
+    .text(city.name)
+    .addClass('showWeather')
+    .appendTo('#searchResults');
+  }
 
   $('#searchWeather').on('click', '.button-primary', function() {
     var userSearch = $("#searchBox").val();
-    console.log(userSearch);
     $.ajax({
       url: 'http://autocomplete.wunderground.com/aq?query=' + userSearch,
       method: 'GET',
       dataType: 'jsonp',
       jsonp:    "cb",
       success:  function (data) {
+        $('#searchResults').empty();
         var i;
         for (i in data.RESULTS) {
-            console.log(data.RESULTS[i]);
+          city = data.RESULTS[i];
+          appendResults(city);
         }
-    }
+      }
     })
     return false;
+  })
+
+  $('#searchResults').on('click', 'button.showWeather', function() {
+    alert('works');
   })
 
 });
