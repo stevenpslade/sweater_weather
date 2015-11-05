e1cbcff99f00ee50b53dafba1c2b47c6cb647fa4
stevenpslade <stevenpslade@gmail.com>
2015-11-05 09:54:28 -0800

    fixed api link bug for search results

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,11 +1,28 @@
 $(function() {
 
+  // var weather = data.current_observation.weather; //string
+  // var temp = data.current_observation.temp_c; //number
+  // var wind = data.current_observation.wind_kph; //number
+  // var feelsLike = data.current_observation.feelslike_c; //string
+  // var icon = data.current_observation.icon_url; //link to image
+
   function appendResults(city) {
     $('<button>')
     .data('link', city.l)
     .text(city.name)
     .addClass('showWeather')
     .appendTo('#searchResults');
+  }
+
+  function appendWeather(data) {
+    var weather = data.weather; //string
+    var temp = data.temp_c; //number
+    var wind = data.wind_kph; //number
+    var feelsLike = data.feelslike_c; //string
+    var icon = data.icon_url; //link to image
+    $('<h2>')
+    .text(temp + 'C')
+    .appendTo('#current');
   }
 
   $('#searchWeather').on('click', '.button-primary', function() {
@@ -28,10 +45,7 @@ $(function() {
   })
 
   $('#searchResults').on('click', 'button.showWeather', function() {
-    var apiLink = city.l + '.json';
-    // http://api.wunderground.com/api/3e541a2c882e2d33/conditions/q/CA/San_Francisco.json
-    //http://api.wunderground.com/api/3e541a2c882e2d33/forecast/q/zmw:94125.1.99999.json
-    //3e541a2c882e2d33
+    var apiLink = $(this).data("link") + '.json';
     $.ajax({
       url: 'http://api.wunderground.com/api/3e541a2c882e2d33/conditions/' + apiLink,
       method: 'GET',
@@ -39,12 +53,9 @@ $(function() {
       // jsonp:    "cb",
       success:  function (data) {
         $('#searchResults').empty();
-        var weather = data.current_observation.weather; //string
-        var temp = data.current_observation.temp_c; //number
-        var wind = data.current_observation.wind_kph; //number
-        var feelsLike = data.current_observation.feelslike_c; //string
-        var icon = data.current_observation.icon_url; //link to image
-        console.log(weather);
+        $('#current').empty();
+        data = data.current_observation;
+        appendWeather(data);
       }
     })
   })
